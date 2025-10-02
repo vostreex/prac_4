75a23c0aa053aaa76419084cb17ffdfafb155178
vostreex <vostan97@gmail.com>
2025-10-02 21:08:11 +0300

    realisation of navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,46 @@
 import 'package:flutter/material.dart';
+import 'screens/separated_screen.dart';
+import 'screens/column_screen.dart';
+import 'screens/listview_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    ColumnScreen(),
+    ListViewScreen(),
+    SeparatedScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<String> items = List.generate(100, (index) => 'Item ${index + 1}');
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Список с ключами')),
-      body: ListView(
-        children: items.map((item) => GestureDetector(
-          key: ValueKey(item),
-          onTap: () {
-            setState(() {
-              items.remove(item); // Удаление элемента при нажатии
-            });
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              item,
-            ),
-          ),
-        )).toList(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Список книг')),
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.view_column), label: 'Column'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'ListView'),
+            BottomNavigationBarItem(icon: Icon(Icons.view_list), label: 'Separated'),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
